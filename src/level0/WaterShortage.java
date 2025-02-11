a9e4e761e7661e2dbb606c99a23a0e1f0f4a34e0
soljjang777 <vivace8520vi@naver.com>
2025-02-11 12:24:37 +0900

    Water Shortage 문제 해결

---
+++ src/level0/WaterShortage.java
@@ -0,0 +1,30 @@
+package level0;
+
+public class WaterShortage {
+
+    public int solution(int storage, int usage, int[] change) {
+        int total_usage = 0;
+        for(int i=0; i<change.length; i++){
+            usage = usage + (usage * change[i]) / 100;
+            System.out.println("usage:" + usage);
+            total_usage += usage;
+            if(total_usage > storage){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        WaterShortage waterShortage = new WaterShortage();
+
+        int storage = 5141;
+        int usage = 500;
+        int[] change = {10, -10, 10, -10, 10, -10, 10, -10, 10, -10};
+        int result = waterShortage.solution(storage, usage, change);
+
+        System.out.println(result);
+
+
+    }
+}
